30e7b0660d009eb41761645c363819b4b7f4745e
islam <islam.quliyev988@gmail.com>
2024-06-17 14:57:35 +0000

    .

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+ *  main - This is the entry point of the code
+ *
+ *  Return: 0 Successful
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
