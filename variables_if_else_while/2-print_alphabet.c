2ca5b35886b2fc847d2672a9255c88261c9427ee
root <islam.quliyev988@gmail.com>
2024-06-19 19:45:48 +0000

    .

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: 0 Successful
+ */
+int main(void)
+{
+
+    for(int l = 0; l < 26; l++)
+        putchar( 'a' + l );
+    putchar( '\n' );
+
+	return (0);
+}
