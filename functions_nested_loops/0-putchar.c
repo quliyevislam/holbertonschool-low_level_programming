9d59305d7dc476c0f035870d9012142e26ee0431
islam <islam.quliyev988@gmail.com>
2024-06-22 07:49:54 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - This is the entry point of the code
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('a');
 
 	return (0);
 }
