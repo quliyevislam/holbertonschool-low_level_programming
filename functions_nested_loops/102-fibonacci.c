20e60f3509d94abbb3b2891701151ebca724f89d
islam <islam.quliyev988@gmail.com>
2024-06-23 07:59:30 +0000

    .

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - .
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+	int F1 = 0;
+	int F2 = 1;
+	int sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = F1 + F2;
+		printf("%lu", sum);
+
+		F1 = F2;
+		F2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
