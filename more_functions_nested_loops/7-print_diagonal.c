ea9c4b8258526ef77edf600026b5b451e774c6b8
islam <islam.quliyev988@gmail.com>
2024-06-26 12:41:22 +0000

    C - More functions, more nested loops

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,15 +5,20 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int j;
-
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
-			_putchar(' ');
-
-		_putchar('\\');
+	if (n < 0)
 		_putchar('\n');
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
