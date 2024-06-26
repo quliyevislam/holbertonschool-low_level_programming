c8c5641a542da202887c4562204172cedaa025d7
islam <islam.quliyev988@gmail.com>
2024-06-26 13:41:44 +0000

    C - More functions, more nested loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,30 +7,34 @@
 int main(void)
 {
 	int i;
+
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 ==0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+
+		if (i != 100)
+			printf(" ");
 	}
-	
+
 	printf("\n");
-	
+
 	return (0);
 }
