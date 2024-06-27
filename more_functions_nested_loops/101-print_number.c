c0121405bedb1190e008bca96f7c03352e59e054
islam <islam.quliyev988@gmail.com>
2024-06-27 19:22:51 +0000

    C - More functions, more nested loops

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -6,36 +6,30 @@
  */
 void print_number(int n)
 {
+	int N;
+	int x = 1;
+	int digit;
+
 	if (n < 0)
 	{
 		n *= -1;
 		_putchar('-');
 	}
 
-	unsigned int reverse = 0;
-	unsigned int temp;
-	unsigned int N;
-	unsigned int new_n = (unsigned int)n;
+	N = n;
+	N = N / 10;
 
-	if (new_n == 0)
-		_putchar('0');
-
-	N = new_n;
-
-	while (new_n != 0)
+	while (N != 0)
 	{
-		temp = new_n % 10;
-		new_n = new_n / 10;
-		reverse  = reverse * 10 + temp;
+		N = N / 10;
+		x = x * 10;
 	}
 
-	while (reverse != 0)
+	while (divider != 0)
 	{
-		temp = reverse % 10;
-		reverse = reverse / 10;
-		_putchar('0' + temp);
+		digit = n / x;
+		n = n - digit * x;
+		x = x / 10;
+		_putchar('0' + digit);
 	}
-
-	if (N % 10 == 0 && N != 0)
-		_putchar('0');
 }
