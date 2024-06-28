abeb4dfa8878863937891071e7859cea4f56f6e1
islam <islam.quliyev988@gmail.com>
2024-06-28 13:55:10 +0000

    C - Pointers, arrays and strings

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * print_array -  prints half of a string, followed by a new line.
- *
+ * 
  * @str: pointer to a str.
  */
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int i = 0;
 
