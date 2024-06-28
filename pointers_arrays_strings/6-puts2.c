79d9171cb99d7704973b08c22b835d325355ff9b
islam <islam.quliyev988@gmail.com>
2024-06-28 11:25:13 +0000

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string.
+ *
+ * @str: pointer to a str;
+ */
+void puts2(char *str)
+{
+	int i = -1;
+
+	do
+	{
+		i++;
+
+		if(i % 2 != 0)
+		continue;
+
+		_putchar(*(str + i));
+	}while (*(str + i) != 0);
+
+	_putchar('\n');
+}
