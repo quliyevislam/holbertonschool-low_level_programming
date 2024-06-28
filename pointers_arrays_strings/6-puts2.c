44709b9feb1517c6326598e7a4f76cc8840de65e
islam <islam.quliyev988@gmail.com>
2024-06-28 12:58:46 +0000

    C - Pointers, arrays and strings

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,19 +6,18 @@
  */
 void puts2(char *str)
 {
-int i = 0;
-int j;
+	int i = 0;
+	int j;
 
-while (*(str + i) != 0 )
-{
-i++;
-}
+	while (*(str + i) != 0)
+	{
+		i++;
+	}
 
-for (j = 0; j < i; j += 2)
-{
-_putchar(*(str + j));
-}
-
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(*(str + j));
+	}
 
 	_putchar('\n');
 }
