7b4d5584255b4e54e0539869df47a3db16aa7a61
islam <islam.quliyev988@gmail.com>
2024-06-28 13:30:44 +0000

    C - Pointers, arrays and strings

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,20 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int j = 0;
-
+	int k;
 	while (*(str + i) != 0)
 	{
 	i++;
 	}
-
-	for (j = i / 2; j < i; j++)
+if (i % 2 == 1)
+{
+	k = (i - 1)/2;
+}
+else
+{
+	k = i/2;
+}
+	for (j = k; j < i; j++)
 	{
 	_putchar(*(str + j));
 	}
