68bcd7db17acc0fabb4de8dd0e3112ec0e292f83
islam <islam.quliyev988@gmail.com>
2024-06-29 22:25:19 +0000

    .

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int len_dest = 0;
+    int len_src = 0;
+    int i;
+    /*while (dest[len_dest] != '\0')
+    {
+        len_dest++;
+    }
+    
+    while (src[i] != '\0')
+    {
+        len_src++;
+    }
+    */
+    for (i = 0; i < n ; i++)
+    {
+        dest[i] = src[i];
+        
+        
+    }
+    
+    /*if (len_src < n)
+    {
+        for (i = len_src; dest[i] != '\0'; i++)
+        {
+            dest[i] = '\0';
+        }
+        
+    }
+    */
+    return(dest);
+}
