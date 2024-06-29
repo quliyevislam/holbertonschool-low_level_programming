b90245989b5f259c6714787ff6047735861e5461
islam <islam.quliyev988@gmail.com>
2024-06-29 22:50:17 +0000

    C - More pointers, arrays and strings

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,29 @@
 #include "main.h"
-
+/**
+ * _strncpy - copies a string.
+ * @dest: pointer.
+ * @src: pointer.
+ * @n: int.
+ * Return: dest.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-    
 	int i;
-for (i = 0; i < n; i++)
-    {
 
-        if (src[i] == '\0')
-        {
-            break;
-        }
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0')
+		{
+		break;
+		}
 
+		dest[i] = src[i];
+	}
 
-    for (; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
-    return(dest);
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
 }
