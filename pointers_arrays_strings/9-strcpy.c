3f3ffbd7e31297ca713149aa7afe3ce89b059124
islam <islam.quliyev988@gmail.com>
2024-06-29 08:24:57 +0000

    .

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
 	int j;
 
-	while (*(src + i) != 0)
-	{
-		i++;
-	}
 
-	for (j = 0; j <= i; j++)
+	for (j = 0; *(src + j) != 0; j++)
 	{
 		dest[j] = *(src + j);
 	}
+
+	dest[j] = '\0';
 
 	return (dest);
 }
