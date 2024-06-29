0ab9b235d32ab32ccad9afad8819872ea57d8488
islam <islam.quliyev988@gmail.com>
2024-06-29 06:54:12 +0000

    C - Pointers, arrays and strings

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array -  prints half of a string, followed by a new line.
+ * print_array - prints n elements of an array of integers, followed by a new line.
  * 
- * @str: pointer to a str.
+ * @a: pointer to a int array.
+ *
+ * @n: int.
  */
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
-	printf("%d", a[i]);
-	if (i != n-1)
-		printf(", ");
+		printf("%d", a[i]);
+
+		if (i != n-1)
+			printf(", ");
 	}
-printf("\n");
 
-
-
+	printf("\n");
 }
