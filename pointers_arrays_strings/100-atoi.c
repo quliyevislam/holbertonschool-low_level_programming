740f223f6f4e7f487ddb0ebbdbc774be67c1dd63
islam <islam.quliyev988@gmail.com>
2024-06-29 16:06:26 +0000

    C - Pointers, arrays and strings

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,10 +16,16 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
-		num = num * 10 + s[i] - '0';
+			num = num * 10 + s[i] - '0';
 
 		if (s[i] == '-')
-		c++;
+			c++;
+
+		if (s[i])
+		{
+			if (s[i] <= '0' && s[i] >= '9')
+			break;
+		}
 	}
 
 	if (c % 2 == 1)
