e813fc486033236d92694bda1ae9ec8ca4075c98
islam <islam.quliyev988@gmail.com>
2024-06-30 18:02:43 +0000

    .

---
+++ pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * infinite_add - Adds two numbers stored as strings.
+ * @n1: The first number.
+ * @n2: The second number.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ * 
+ * Return: A pointer to the result if successful, or 0 if it fails.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int i, j, k;
+    int carry = 0;
+    int sum;
+    
+    if (size_r <= len1 || size_r <= len2)
+        return 0;
+
+    r[size_r - 1] = '\0';
+    k = size_r - 2;
+
+    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--, k--) {
+        if (k < 0)
+            return 0;
+        
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i] - '0';
+        if (j >= 0)
+            sum += n2[j] - '0';
+
+        carry = sum / 10;
+        r[k] = (sum % 10) + '0';
+    }
+
+    if (k < 0)
+        return 0;
+
+    if (r[k] == '0')
+        k++;
+
+    return r + k;
+}
+
