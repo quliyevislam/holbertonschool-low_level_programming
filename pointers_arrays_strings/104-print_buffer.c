a0949b250cf072d76259e8210039caaf2d5970ca
islam <islam.quliyev988@gmail.com>
2024-06-30 18:36:18 +0000

    C - More pointers, arrays and strings

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
-
 /**
  * print_buffer - Prints a buffer.
- * @b: The buffer to print.
- * @size: The size of the buffer.
+ * @b: pointer to a char.
+ * @size: int.
  */
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
+	if (size <= 0)
+	{
+		rintf("\n");
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-                printf("%02x", b[i + j]);
-            else
-                printf("  ");
-            if (j % 2 != 0)
-                printf(" ");
-        }
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", b[i + j]);
 
-        for (j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-            {
-                if (isprint(b[i + j]))
-                    printf("%c", b[i + j]);
-                else
-                    printf(".");
-            }
-        }
+			else
+				printf("  ");
 
-        printf("\n");
-    }
+			if (j % 2 != 0)
+				printf(" ")
+		}
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (isprint(b[i + j]))
+					printf("%c", b[i + j]);
+
+				else
+					printf(".");
+			}
+		}
+
+		printf("\n");
+	}
 }
-
