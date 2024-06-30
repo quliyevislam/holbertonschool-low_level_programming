60f9779e1aa029e5f09479ffc0926218e2cf4e44
islam <islam.quliyev988@gmail.com>
2024-06-30 08:19:23 +0000

    C - More pointers, arrays and strings

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -6,6 +6,8 @@
  */
 char *rot13(char *s)
 {
+	int i;
+	int j;
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
