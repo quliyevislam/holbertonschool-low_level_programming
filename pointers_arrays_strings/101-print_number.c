7a986526e193f07a62c6581b60113e71bee797dd
islam <islam.quliyev988@gmail.com>
2024-06-30 08:38:34 +0000

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ *
+ * @n: int.
+ */
+void print_number(int n)
+{
+
+int N;
+	int x = 1;
+	int digit;
+
+	if (n == -2147483648)
+	{
+	putchar('-');
+	putchar('2');
+	n = 147483648;
+	}
+
+	if (n < 0)
+	{
+		n *= -1;
+		putchar('-');
+	}
+
+	N = n;
+	N = N / 10;
+
+	while (N != 0)
+	{
+		N = N / 10;
+		x = x * 10;
+	}
+
+	while (x != 0)
+	{
+		digit = n / x;
+		n = n - digit * x;
+		x = x / 10;
+		putchar('0' + digit);
+	}
+}
