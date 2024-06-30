fffcb253c0a4eee9a90b56d38bc7541706d4e0d2
islam <islam.quliyev988@gmail.com>
2024-06-30 18:13:40 +0000

    .

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -18,11 +18,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
     int carry = 0, sum;
     int i = len1 - 1, j = len2 - 1, k = size_r - 2;
 
-    if (size_r <= len1 + 1 || size_r <= len2 + 1 || size_r < len1 + len2 + 2)
+    
+    if (size_r <= len1 + 1 || size_r <= len2 + 1)
         return 0;
 
     r[size_r - 1] = '\0';
-
+    
     while (i >= 0 || j >= 0 || carry)
     {
         if (k < 0)
@@ -40,9 +41,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
     k++;
 
-    if (k < 0 && (size_r - 1 - k) > size_r)
-        return 0;
+    if (r[k] == '0')
+        k++;
 
     return r + k;
 }
-
