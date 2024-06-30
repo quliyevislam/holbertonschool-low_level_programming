b7aeac144e30350e67d60d368dbc2f3bc9d89db5
islam <islam.quliyev988@gmail.com>
2024-06-30 08:17:41 +0000

    C - More pointers, array

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - check the code.
+ * @s: pointer.
+ * Return: s.
+ */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
