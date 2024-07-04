8b85ad69c8321818ea593c3acfde22eb793e4d31
islam <islam.quliyev988@gmail.com>
2024-07-04 09:36:18 +0000

    C - Even more pointers, arrays and strings

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * _memset -  fills memory with a constant byte.
- * @s: pointer to a char.
- * @b: char.
+ * @dest: pointer to a char.
+ * @src: pointer to a char.
  * @n: unsigned int.
  * Return: s.
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		dest[i] = src[i];
 	}
 
-	return (s);
+	return (dest);
 }
