92cf3e9ebee506c30ed642376731a5b33988d455
islam <islam.quliyev988@gmail.com>
2024-07-04 13:41:45 +0000

    .

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int j;
+int k = 0;
+
+for (i = 0; haystack[i]; i++)
+{
+    for (j = 0; needle[j]; j++)
+    {
+        
+        if (haystack[i + j] == needle[j])
+        k++;
+        
+    }
+    if (k == j)
+    return haystack + i;
+   
+    k = 0;
+}
+    
+    
