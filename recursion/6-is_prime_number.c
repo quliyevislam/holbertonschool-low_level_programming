5389c6d0a8a2934b1e0efe420f46a6194b6993a3
islam <islam.quliyev988@gmail.com>
2024-07-07 10:36:38 +0000

    C - Recursion

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime -  if the input integer is a prime number ?
+ * @n: int.
+ * @k: int.
+ * Return: 1 if the input integer is prime.
+ */
+int is_prime(int n , int k)
+{
+	if (n == k)
+		return (1);
+
+	if (n % k == 0)
+		return (0);
+
+	return is_prime(n, k + 1);
+}
+/**
+ * is_prime_number -  if the input integer is a prime number ?
+ * @n: int.
+ * Return: 1 if the input integer is prime.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return is_prime(n, 2);
+}
