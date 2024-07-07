94acd314d84808026ba4112d2590d7910dad1a66
islam <islam.quliyev988@gmail.com>
2024-07-07 10:13:40 +0000

    C - Recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _sqrt_r - returns the natural square root of a number.
+ * @S: int.
+ * @x: int.
+ * Return: natural square root of a number.
+ */
+int _sqrt_r(int S, int x)
+{
+	if (x * x <= S)
+		return (x);
+
+	return (_sqrt_r(S, (x + S / x) / 2));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: int.
+ * Return: natural square root of a number.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt_r(n, n));
+}
