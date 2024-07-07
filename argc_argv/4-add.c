b4c6b3333ffed3e1b8d8e621e4c85bd9e13bc65f
islam <islam.quliyev988@gmail.com>
2024-07-07 16:04:09 +0000

    C - argc, argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,14 +10,14 @@ int i;
 int j;
 int sum = 0;
 char *str;
-if (arcc == 1)
+if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
 for (i = 1; i < argc; i++)
 {
-srt = argv[i]
+srt = argv[i];
 for (j = 0; str[j]; j++)
 {
 if (str[j] < '0' && str[j] > '9')
@@ -32,7 +32,7 @@ for (i = 1; i < argc; i++)
 {
 sum = sum + atoi(argv[i]);
 }
-printf("%d\n");
+printf("%d\n", sum);
 
 return (0);
 }
