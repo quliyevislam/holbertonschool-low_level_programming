35786b32acfe1180454e595d0b7590172f4816dc
islam <islam.quliyev988@gmail.com>
2024-07-07 14:22:50 +0000

    C - argc, argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
 
 	int i;
 
-	for (i = 0;**argv[i] && i < argc; i++)
+	for (i = 0; i < argc && *argv[i]; i++)
 
-	printf("%d", i + 1);
+	printf("%d\n", i + 1);
 
 	return (0);
 }
