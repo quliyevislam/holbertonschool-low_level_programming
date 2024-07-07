b7839b891a4e0fa8a2f14b6bb43a4f15addfe2c7
islam <islam.quliyev988@gmail.com>
2024-07-07 14:07:52 +0000

    C - argc, argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: int.
+ * Return: 0.
+ */
+int main(int argc)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
