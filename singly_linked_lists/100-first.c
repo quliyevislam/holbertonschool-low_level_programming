4d422b76bea989ba1458acdfbc911e784263ab31
Islam <9337@holbertonstudents.com>
2024-08-02 21:04:09 +0400

    Add 100-first.c file

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * myStartupFun -  prints
+ * "You're beat! and yet, you must allow,
+ * \nI bore my house upon my back!\n"
+ * before the main function is executed.
+ */
+void myStartupFu(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
