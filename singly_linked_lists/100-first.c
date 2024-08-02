e01b6cebdee887907fe69a20e3b21cbb770f809d
Islam <9337@holbertonstudents.com>
2024-08-02 21:20:41 +0400

    Added function prototype
    `void myStartupFun(void) __attribute__((constructor));`

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+void myStartupFun (void) __attribute__ ((constructor));
+
 /**
  * myStartupFun -  prints
  * "You're beat! and yet, you must allow,
  * \nI bore my house upon my back!\n"
  * before the main function is executed.
  */
-void myStartupFu(void)
+void myStartupFUN(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
