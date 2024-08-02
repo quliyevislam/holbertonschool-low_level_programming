27d20e6fb7003f9ef336d4884f31144598a9db0a
Islam <9337@holbertonstudents.com>
2024-08-02 18:20:19 +0400

    Removed `strlen` and added custom `_strlen`
    Removed `strlen` function and added custom `_strlen` function to the 2-add_node.c file

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - calculates the length of a string
+ * @str: the input string to be measured
+ *
+ * Return: the length of the input string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int len = 0
+
+	while (*str)
+		str++, len++;
+
+	return (len);
+}
+
 /**
  * add_node - adds a new node at the beginning of a `list_t` list
  * @head: pointer to the head pointer of the list
@@ -15,7 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 
 	*head = new_node;
