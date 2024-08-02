460633ff1d68a028420e37d2e451d875cd859484
Islam <9337@holbertonstudents.com>
2024-08-02 16:49:54 +0400

    Fixed bug by adding `else`

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
-
-		printf("[%u] %s", h->len, h->str);
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		number_of_nodes++;
