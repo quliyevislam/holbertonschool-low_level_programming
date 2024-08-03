ba69499046958b5094c074fdab071c82c9e44491
Islam <9337@holbertonstudents.com>
2024-08-03 19:45:07 +0400

    Testing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,7 +7,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *temp_prev;
     unsigned int  i = 0;
 
-    if (!temp_next && !idx)
+    if (!idx)
         return(add_dnodeint(h, n));
     
     
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     temp_prev = temp_next->prev;    
     
     if (!temp_prev)
-        return (add_dnodeint(h, n));
+        return (add_dnodeint(&temp_next, n));
 
     temp_prev->next = add_dnodeint(&temp_next, n);
     (temp_prev->next)->prev = temp_prev;
