fa8653e8135f272735ab28930ff7a436013ca668
Islam <9337@holbertonstudents.com>
2024-08-03 14:41:23 +0400

    Fixed bug

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 
 	if (!(*head))
-		new_node->prev = *head;
+	{	
+		*head = new_node;
+	        (*head)->perv = NULL;
+	}
 
 	else 
 	{
