5c2ac3926b4186350aa86bbf3b021da5b182dca8
Islam <9337@holbertonstudents.com>
2024-08-03 13:22:45 +0400

    Removed extra variables; fixed bug by removing `;`

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *temp = h;
 	unsigned int  number_of_nodes = 0;
 
-	while (temp)
+	while (h)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		temp = h->next;
 		number_of_nodes++;
 	}
 
--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -7,7 +7,7 @@
  *
  * Return: the number of nodes
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	unsigned int  number_of_elements = 0;
 
