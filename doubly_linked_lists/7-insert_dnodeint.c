8c099c9a648968529a2ce8399066734bd7fff2ef
Islam <9337@holbertonstudents.com>
2024-08-03 19:46:49 +0400

    Testing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,29 +1,34 @@
 #include "lists.h"
 #include <stdlib.h>
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *temp_next = *h;
-    dlistint_t *temp_prev;
-    unsigned int  i = 0;
+    dlistint_t *new_node, *temp = *h;
+    unsigned int i = 0;
 
-    if (!idx)
-        return(add_dnodeint(h, n));
-    
-    
-    while (i < idx && temp_next)
-            temp_next = temp_next->next, i++;
+    if (idx == 0)
+        return add_dnodeint(h, n);
 
-    if (!temp_next)
-        return (NULL);
-    
-    temp_prev = temp_next->prev;    
-    
-    if (!temp_prev)
-        return (add_dnodeint(&temp_next, n));
+    new_node = malloc(sizeof(dlistint_t));
+    if (!new_node)
+        return NULL;
 
-    temp_prev->next = add_dnodeint(&temp_next, n);
-    (temp_prev->next)->prev = temp_prev;
-    
-    return (temp_prev->next);
+    new_node->n = n;
+
+    while (temp && i < idx - 1) {
+        temp = temp->next;
+        i++;
+    }
+
+    if (!temp)
+        return NULL;
+
+    new_node->next = temp->next;
+    new_node->prev = temp;
+
+    if (temp->next)
+        temp->next->prev = new_node;
+
+    temp->next = new_node;
+
+    return new_node;
 }
