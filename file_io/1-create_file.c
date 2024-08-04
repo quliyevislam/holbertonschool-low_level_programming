6e1b75214596e1f2af4382f6a64e973e39e4dcc1
Islam <9337@holbertonstudents.com>
2024-08-04 17:55:12 +0400

    Fixed bugs

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,32 +4,32 @@
 #include <stdio.h>
 /**
  * create_file - creates a file.
- * @file: the file name
- * @text: string of text for the new file
+ * @filename: the file name
+ * @text_content: string of text for the new file
  *
  * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
  */
-int create_file(const char *file, char *text)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int w;
 	int l = 0;
 	int c;
 
-	if (file == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (text != NULL)
+	if (text_content != NULL)
 	{
-		while (text[l])
+		while (text_content[l])
 			l++;
 
-		w = write(fd, text, l);
+		w = write(fd, text_content, l);
 
 		if (w == -1)
 		{
