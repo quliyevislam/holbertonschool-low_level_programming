a9902eaedca2b849b125d5f651566411b105f473
Islam <9337@holbertonstudents.com>
2024-08-04 18:38:49 +0400

    testing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,9 +1,19 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-
-int exit_func(int error, char *name, int fd)
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * __exit - prints error messages and exits with exit value
+ * @error: num is either exit value or file descriptor
+ * @s: str is a name, either of the two filenames
+ * @fd: file descriptor
+ * Return: 0 on success
+ **/
+int __exit(int error, char *s, int fd)
 {
 	switch (error)
 	{
@@ -11,10 +21,10 @@ int exit_func(int error, char *name, int fd)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(error);
 	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
 		exit(error);
 	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
 		exit(error);
 	case 100:
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -24,33 +34,39 @@ int exit_func(int error, char *name, int fd)
 	}
 }
 
+/**
+ * main - copies one file to another
+ * @argc: should be 3 (./a.out copyfromfile copytofile)
+ * @argv: first is file to copy from (fd_1), second is file to copy to (fd_2)
+ * Return: 0 (success), 97-100 (exit value errors)
+ */
 int main(int argc, char *argv[])
 {
-	int fd_1, fd_2, r, w;
+	int fd_1, fd_2, n_read, n_wrote;
 	char *buffer[1024];
 
 	if (argc != 3)
-		exit_func(97, NULL, 0);
+		__exit(97, NULL, 0);
 
 	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_2 == -1)
-		exit_func(99, argv[2], 0);
+		__exit(99, argv[2], 0);
 
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 == -1)
-		exit_func(98, argv[1], 0);
+		__exit(98, argv[1], 0);
 
-	while ((r = read(fd_1, buffer, 1024)) != 0)
+	while ((n_read = read(fd_1, buffer, 1024)) != 0)
 	{
-		if (r == -1)
-			exit_func(98, argv[1], 0);
+		if (n_read == -1)
+			__exit(98, argv[1], 0);
 
-		w = write(fd_2, buffer, r);
-		if (w == -1)
-			exit_func(99, argv[2], 0);
+		n_wrote = write(fd_2, buffer, n_read);
+		if (n_wrote == -1)
+			__exit(99, argv[2], 0);
 	}
 
-	close(fd_2) == -1 ? (exit_func(100, NULL, fd_2)) : close(fd_2);
-	close(fd_1) == -1 ? (exit_func(100, NULL, fd_1)) : close(fd_1);
+	close(fd_2) == -1 ? (__exit(100, NULL, fd_2)) : close(fd_2);
+	close(fd_1) == -1 ? (__exit(100, NULL, fd_1)) : close(fd_1);
 	return (0);
 }
