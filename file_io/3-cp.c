43dd512366a3cc69b16e2443aac0a02fc441fa32
Islam <9337@holbertonstudents.com>
2024-08-04 18:24:03 +0400

    Testing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,46 +2,45 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 int main(int argc, char **argv)
 {
-	int o, o_2, w;
+	int fd, fd2, filecheck;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	o_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (o_2 == -1)
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((w = read(o, buffer, 1024)) > 0)
+	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
-		if (w == -1)
+		if (filecheck == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		w = write(o_2, buffer, w);
-		if (w == -1)
+		filecheck = write(fd2, buffer, filecheck);
+		if (filecheck == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (w == -1)
+	if (filecheck == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(o) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o), exit(100);
-	if (close(o_2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o_2), exit(100);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 
 	return (0);
 }
