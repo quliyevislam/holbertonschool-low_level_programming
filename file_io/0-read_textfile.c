70fc066cb438575109260e29d8af731f05d89615
Islam <9337@holbertonstudents.com>
2024-08-04 15:36:01 +0400

    Testing

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,21 +1,46 @@
 #include <fcntl.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+    int fd;
+    ssize_t read_count, write_count;
+    char *buffer;
 
-int fd;
-ssize_t n;
-char *buf = malloc(sizeof(char) * letters);
+    if (filename == NULL)
+        return (0);
 
-fd = open(filename, O_RDONLY);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-n = read(fd, buf, letters);
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-close(fd);
-write(1, buf, letters);
+    read_count = read(fd, buffer, letters);
+    if (read_count == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
 
-return (n);
+    write_count = write(STDOUT_FILENO, buffer, read_count);
+    if (write_count == -1 || write_count != read_count)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+    return (write_count);
 }
+
