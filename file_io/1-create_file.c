618fd268f810ff63f0bccf448edff767d69fe42e
Islam <9337@holbertonstudents.com>
2024-08-04 17:40:57 +0400

    Betty fixed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,35 +2,40 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * create_file - creates a file.
+ * @file: the file name
+ * @text: string of text for the new file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ */
 int create_file(const char *file, char *text)
 {
-    int fd, w, l, c;
-    mode_t m = S_IRUSR | S_IWUSR;
+	int fd, w, l, c;
 
-    if (file == NULL)
-        return (-1);
+	if (file == NULL)
+		return (-1);
 
-    fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, m);
-    if (fd == -1)
-        return (-1);
+fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    if (text != NULL)
-    {
-        for (l = 0; text[l] != '\0'; l++)
-            ;
-        w = write(fd, text, l);
-        if (w == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text != NULL)
+	{
+		for (l = 0; text[l] != '\0'; l++)
+			w = write(fd, text, l);
 
-    c = close(fd);
-    if (c == -1)
-        return (-1);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    return (1);
+	c = close(fd);
+	if (c == -1)
+		return (-1);
+
+	return (1);
 }
-
