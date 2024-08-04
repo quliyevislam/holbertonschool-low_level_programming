9e2498eb5cef1d8ac9d474253e655a23adafce6c
Islam <9337@holbertonstudents.com>
2024-08-04 18:37:18 +0400

    testing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int _exit(int error, char *name, int fd)
+int exit_func(int error, char *name, int fd)
 {
 	switch (error)
 	{
@@ -30,27 +30,27 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-		_exit(97, NULL, 0);
+		exit_func(97, NULL, 0);
 
 	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_2 == -1)
-		_exit(99, argv[2], 0);
+		exit_func(99, argv[2], 0);
 
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 == -1)
-		_exit(98, argv[1], 0);
+		exit_func(98, argv[1], 0);
 
 	while ((r = read(fd_1, buffer, 1024)) != 0)
 	{
 		if (r == -1)
-			_exit(98, argv[1], 0);
+			exit_func(98, argv[1], 0);
 
 		w = write(fd_2, buffer, r);
 		if (w == -1)
-			_exit(99, argv[2], 0);
+			exit_func(99, argv[2], 0);
 	}
 
-	close(fd_2) == -1 ? (_exit(100, NULL, fd_2)) : close(fd_2);
-	close(fd_1) == -1 ? (_exit(100, NULL, fd_1)) : close(fd_1);
+	close(fd_2) == -1 ? (exit_func(100, NULL, fd_2)) : close(fd_2);
+	close(fd_1) == -1 ? (exit_func(100, NULL, fd_1)) : close(fd_1);
 	return (0);
 }
