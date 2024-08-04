a5bd3b2457a6a5f7b97b3943f793eb8efbf04994
Islam <9337@holbertonstudents.com>
2024-08-04 12:41:01 +0400

    Add 4-clear_bit.c

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -7,12 +7,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bit = 1;
 
 	if (index > 64)
 		return (-1);
 
-	*n = (bit << index ^ *n) & *n;
+	*n = (1UL << index ^ *n) & *n;
 
 	return (1);
 }
