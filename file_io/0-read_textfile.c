d9276a1c632e07f14ac7d11c9710511abebc9cd1
Islam <9337@holbertonstudents.com>
2024-08-04 16:55:49 +0400

    Fiex all problems

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,42 +14,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t o;
-    ssize_t w, r;
-    char *buffer;
+	ssize_t o;
+	ssize_t w, r;
+	char *buffer;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    o = open(filename, O_RDONLY);
-    if (o == -1)
-        return (0);
+	o = open(filename, O_RDONLY);
+	if (o == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(o);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(o);
+		return (0);
+	}
 
-    r = read(o, buffer, letters);
-    if (r == -1)
-    {
-        free(buffer);
-        close(o);
-        return (0);
-    }
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
 
-    w = write(STDOUT_FILENO, buffer, r);
-    if (w == -1 || w != r)
-    {
-        free(buffer);
-        close(o);
-        return (0);
-    }
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
 
-    free(buffer);
-    close(o);
-
-    return (w);
+	free(buffer);
+	close(o);
+	return (w);
 }
