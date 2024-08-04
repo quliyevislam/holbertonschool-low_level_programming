31e9890d1585b87831f1608f9f0372e72b6fc78e
Islam <9337@holbertonstudents.com>
2024-08-04 16:40:27 +0400

    Testing

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,45 +2,47 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: the name of the file
+ * @letters: size
+ *
+ * Return: actual number of letters it could read
+ * and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t read_count, write_count;
-    char *buffer;
+	int o;
+	ssize_t n;
+	char *buffer;
+	
+	if (!filename)
+		return (0);
+	
+	o = open(filename, O_RDONLY);
+	
+	if(o == -1)
+		return (0);
+	
+	buffer = malloc(sizeof(char) * letters);
 
-    if (filename == NULL)
-        return (0);
+	if (read(o, buffer,letters) == -1)
+	{	
+		free(buffer);
+		return (0);
+	}
+	
+	n = write(1,buffer,letters);
+	
+	if (n == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	close(o);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-
-    read_count = read(fd, buffer, letters);
-    if (read_count == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    write_count = write(STDOUT_FILENO, buffer, read_count);
-    if (write_count == -1 || write_count != read_count)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    free(buffer);
-    close(fd);
-    return (write_count);
+	return (n);
 }
 
